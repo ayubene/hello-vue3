1c5a0fda517dda44a5ffd93ac18c0766ca68baf1
ayubene <wubq37@gmail.com>
2024-03-09 10:58:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <Person ref="temp"/>
+    <button @click="showLog">test</button>   
+</template>
+
+<script lang="ts" setup name="App">
+    import Person from './components/Person.vue';
+    import {ref} from 'vue'
+    // export default {
+    //     name:'App',
+    //     components:{Person}
+    // }
+
+    let temp = ref()
+
+    // 如果components的文件内不使用defineExpose，是无法获取到组件标签内的数据的
+    function showLog(){
+        console.log(temp.value);
+    }
+</script>
---
+++ src/components/Person.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="person">
+        <h1>this</h1>
+        <h2 ref="title">that</h2>
+        <button @click="showLog">changeTemp</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person">
+    import {ref,defineExpose} from 'vue'
+    
+    let title = ref()
+
+    let a = ref(0)
+    let b = ref(1)
+    let c = ref(2)
+
+    function showLog(){
+        console.log(title.value); 
+    }
+
+    // 将数据a,b暴露给根组件，隐藏c
+    defineExpose({a,b})
+   
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    
+    button {
+        margin : 0 5px;
+    }
+
+    li {
+        font-size : 20px;
+    }
+</style>
